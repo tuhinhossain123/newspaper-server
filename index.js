d214b3ebe267d68759a88db5f8f77732999cae70
tuhinhossain123 <csetuhin55@gmail.com>
2023-11-28 22:55:51 +0600

    last commit

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
       res.send(result)
      })
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
